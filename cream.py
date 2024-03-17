f28cfa7fd0c3198fe455fe39445d2a36a8050c9c
Mikhail Boin <138712459+Mis0in@users.noreply.github.com>
2024-03-17 12:28:23 +0300

    more detailed stats added

--- cream.py
+++ cream.py
@@ -16,6 +16,10 @@ def test():
     test_f = open(file_name, "r")
     tests = test_f.readlines()
 
+    half = 0
+    single = 0
+    half_pass = 0
+    single_pass = 0
     count = 0
     passed = 0
     for test_id in range(0, len(tests)):
@@ -28,17 +32,30 @@ def test():
         arg = [path] + arg
         output = subprocess.check_output(arg).decode("utf-8")
         count += 1
+        if (arg[1] == 'h'):
+            half += 1
+        elif (arg[1] == 'f'):
+            single += 1
         if res.strip('\r\n ') != output.strip('\r\n '):
-            print("FAILED test: ")
+            print("FAILED test: " + str(test_id))
             for i in range(1, len(arg)):
                 print(arg[i], end=" ")
             print("Expected:", res, end=" ")
             print("Actual:", output)
         else:
+            if (arg[1] == 'h'):
+                half_pass += 1
+            elif (arg[1] == 'f'):
+                single_pass += 1
             passed += 1
-            print("PASSED")
+            #print("PASSED")
 
-        if (count % 10000 == 0):
-            print(str(count) + " tests pass")
-    print(str(passed) + "/" + str(count) + "PASSED")
+        if (count % 1000 == 0):
+            print('----------------------------------')
+            print("STATS:" + str(passed) + '/' + str(count) )
+            print("HALF PASSED: " + str(half_pass) + "/" + str(half))
+            if single > 0:
+                print("SINGLE PASSED: " + str(single_pass) + "/" + str(single))
+            print('----------------------------------')
+    print(str(passed) + "/" + str(count) + " PASSED")
 test()
